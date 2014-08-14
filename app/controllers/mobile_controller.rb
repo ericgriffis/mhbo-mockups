3ee4549e6f6daea93cc62ef1741eb5161289e03c
whroman <me@walterroman.com>
2014-08-14 13:15:15 -0700

    Updated project so as to not be indexed by search engines

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -32,6 +32,9 @@ GFY_BTNS = [
 ]
 
 class MobileController < ApplicationController
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
     def homeV6a
     end
 
